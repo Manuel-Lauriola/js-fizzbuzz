4eb04e00ea0cd1d6532c1c5b03ddb867b397ca2d
Manuel-Lauriola <manuelauriol@gmail.com>
2025-07-02 16:15:32 +0200

    [UPD]-2/5

--- js/script.js
+++ js/script.js
@@ -2,4 +2,16 @@
 
 for(let i = 0; i<=100; i++) {
   console.log(i)
+// Per prima cosa assegno Fizzbuzz ai multipli comuni
+  if(i % 15 === 0){
+    let i = "Fizzbuzz"
+    console.log(i)
+  }
+
+
 }
+
+
+
+
+
