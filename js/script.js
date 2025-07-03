361fd6a7aa3a61078889ca3bea7ae00dd2d9b75a
Manuel-Lauriola <manuelauriol@gmail.com>
2025-07-03 09:58:34 +0200

    [FIX]

--- js/script.js
+++ js/script.js
@@ -5,16 +5,13 @@ for(let i = 1; i<=100; i++) {
   // Per prima cosa assegno Fizzbuzz ai multipli comuni
   if(i % 15 === 0){
     message = "FizzBuzz"
-    // console.log(i)
   }
   //procedo poi per i multipli esclusivi di 3 e 5
   else if(i % 5 === 0){
     message = "Buzz"
-    // console.log(i)
   } 
   else if(i % 3 === 0){
     message = "Fizz"
-    // console.log(i)
   }
   // infine stampo i numeri che non sono multipli di 3 o 5
   console.log(message)
