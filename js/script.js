f323da0cfa4923890dc9d3155c407e36d186df51
Manuel-Lauriola <manuelauriol@gmail.com>
2025-07-03 09:34:09 +0200

    [FIX]

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
 // Scrivi un programma che stampi i numeri da 1 a 100
 
 for(let i = 1; i<=100; i++) {
-
-// Per prima cosa assegno Fizzbuzz ai multipli comuni
+  let message = i
+  // Per prima cosa assegno Fizzbuzz ai multipli comuni
   if(i % 15 === 0){
-    let i = "FizzBuzz"
-    console.log(i)
+    message = "FizzBuzz"
+    // console.log(i)
   }
   //procedo poi per i multipli esclusivi di 3 e 5
   else if(i % 5 === 0){
-    let i = "Buzz"
-    console.log(i)
-  }
+    message = "Buzz"
+    // console.log(i)
+  } 
   else if(i % 3 === 0){
-    let i = "Fizz"
-    console.log(i)
+    message = "Fizz"
+    // console.log(i)
   }
   // infine stampo i numeri che non sono multipli di 3 o 5
-    else {
-    console.log(i)
-    }
-  }
+  // else {
+  //   console.log(i)
+  //   }
+  console.log(message)
+}
